173f77cbd58b5513fa23e8499b530fd5f83222f7
369webforall <369webforall@gmail.com>
2024-04-22 15:26:22 +0545

    active link

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,7 +1,13 @@
+'use client'
 import React from 'react'
 import Link from 'next/link'
 import { Bug } from 'lucide-react'
+import classNames from 'classnames'
+import { usePathname } from 'next/navigation'
+
 const Navbar = () => {
+    const currentPath = usePathname();
+  
     const links = [{id:1, label:'Dashboard', href:'/dashboard'}, {id:2, label:'Issues', href:'/issues/view'}]
   return (
     <header className='mx-4 border-b py-2'>
@@ -10,7 +16,11 @@ const Navbar = () => {
 
         <ul className='flex items-center gap-4'>
         {links.map((item)=>(
-            <li key={item.id}><Link href={item.href}>{item.label}</Link></li>
+            <li key={item.id}><Link href={item.href} className={classNames({
+                "text-gray-900": currentPath === item.href,
+                "text-gray-500":currentPath !== item.href,
+                "hover:text-purple-800 transition-all": true
+            })}>{item.label}</Link></li>
         ))}
         </ul>
         </nav>
