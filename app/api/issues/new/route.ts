aa0a1c70c1a6f1f341128b370a5f4b88b85a5aff
369webforall <369webforall@gmail.com>
2024-04-24 14:20:39 +0545

    build api

--- app/api/issues/new/route.ts
+++ app/api/issues/new/route.ts
@@ -1,15 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
+import {z} from 'zod';
 
 // http://localhost:3000/api/issues/new
 
+const IssueFormSchema = z.object({
+    title:z.string().min(1, {message:"Title is required"}).max(50),
+    description:z.string().min(6, {message:"Description is required"}).max(2000)
+}) 
+
+
 export async function POST(request:NextRequest){
 const body = await request.json();
-if(!body){
-    return NextResponse.json({error:"No data found"}, {status:400})
+const validation = IssueFormSchema.safeParse(body)
+
+if(!validation.success){
+    return NextResponse.json(validation.error.format(), {status:400})
 }
 
-const issue = prisma.issue.create({
+const issue = await prisma.issue.create({
     data:{
         title:body.title,
         description:body.description,
