170670b437505262738aa2228af1a0ab31ce6558
369webforall <369webforall@gmail.com>
2024-04-25 14:46:24 +0545

    Implementing client side valiation

--- app/api/issues/new/route.ts
+++ app/api/issues/new/route.ts
@@ -1,14 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
-import {z} from 'zod';
-
-// http://localhost:3000/api/issues/new
-
-const IssueFormSchema = z.object({
-    title:z.string().min(1, {message:"Title is required"}).max(50),
-    description:z.string().min(6, {message:"Description is required"}).max(2000)
-}) 
-
+import { IssueFormSchema } from "../../../ValidationSchema";
 
 export async function POST(request:NextRequest){
 const body = await request.json();
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,19 +1,25 @@
 'use client'
 import React, {useState} from 'react'
-import { Button, TextField, Callout } from '@radix-ui/themes';
+import { Button, TextField, Callout, Text } from '@radix-ui/themes';
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import {useForm, Controller} from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
-interface IssueFormData {
-  title:string,
-  description:string,
-}
+import { zodResolver } from '@hookform/resolvers/zod';
+import {z} from 'zod';
+import { IssueFormSchema } from '@/app/ValidationSchema';
+// interface IssueFormData {
+//   title:string,
+//   description:string,
+// }
+
+type IssueFormData =z.infer<typeof IssueFormSchema>
 
 const NewIssue = () => {
   const router = useRouter();
-  const {register, control, handleSubmit, formState:{errors}} = useForm<IssueFormData>();
+  const {register, control, handleSubmit, formState:{errors}} = useForm<IssueFormData>({
+    resolver:zodResolver(IssueFormSchema)});
 const[error, setError]=useState("")
 
   const onSubmit = handleSubmit(async(data)=>{
@@ -31,14 +37,13 @@ const[error, setError]=useState("")
     <Callout.Text>{error}</Callout.Text>
       </Callout.Root>}
     <form className='max-w-lg space-y-4 mt-5' onSubmit={onSubmit}>
-      <TextField.Root placeholder='Title...' {...register('title')}>
-     
-      </TextField.Root>
+      <TextField.Root placeholder='Title...' {...register('title')}></TextField.Root>
+      {errors.title?.message && <Text as='p' color='red' my="4">{errors.title.message}</Text>  }
       <Controller 
       name="description" 
       control={control} 
       render={({field})=><SimpleMDE placeholder="Reply to comment…"  {...field}/>} />
-      
+       {errors.description?.message && <Text as='p' color='red' my="4">{errors.description.message}</Text>  }
      <Button type='submit'>Submit Issue</Button>
     </form>
     </div>
