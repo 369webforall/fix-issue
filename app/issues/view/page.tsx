d032dfa9b2767c231a5790122299e8a6fc71551f
369webforall <369webforall@gmail.com>
2024-04-30 08:53:02 +0545

    add skeleton

--- app/issues/view/page.tsx
+++ app/issues/view/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import prisma from '@/prisma/client'
 import { Table } from '@radix-ui/themes';
 import IssueBadge from '@/app/components/IssueBadge';
+import delay from 'delay';
 const IssuePage = async() => {
+  await delay(2000)
   const issues = await prisma.issue.findMany();
   return (
     <div>
