60aade962362e4d5cf8a58a5d69ba6c750c81fb4
369webforall <369webforall@gmail.com>
2024-04-30 14:20:25 +0545

    SRP

--- app/issues/view/[id]/page.tsx
+++ app/issues/view/[id]/page.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
 import prisma from '@/prisma/client'
+import { Box, Grid } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
-import IssueBadge from '@/app/components/IssueBadge'
-import { Flex, Heading, Card, Box, Text, Button, Grid } from '@radix-ui/themes'
-import Markdown from 'react-markdown'
-import Link from 'next/link'
-const IssueDetails = async({params}:{params:{id:string}}) => {
+import EditIssueButton from '../_components/EditIssueButton'
+import IssueDetails from '../_components/IssueDetails'
+const IssueDetailsPage = async({params}:{params:{id:string}}) => {
     let issue = await prisma.issue.findUnique({
         where:{
             id:params.id
@@ -15,20 +13,13 @@ const IssueDetails = async({params}:{params:{id:string}}) => {
   return (
     <Grid columns={{initial:'1', sm:'2'}} gap="5" justify="center" align="center">
     <Box as='div' className='space-y-4'>
-        <Heading as='h1'>{issue.title}</Heading>
-        <Flex className='space-x-4' mt="4">
-        <IssueBadge status={issue.status}/>
-        <Text as='p'>{issue.createdAt.toDateString()}</Text>
-        </Flex>
-        <Card className='prose'>
-        <Markdown>{issue.description}</Markdown>
-        </Card> 
+        <IssueDetails issue={issue} />
     </Box>
     <Box>
-<Button><Link href={`/issues/view/${issue.id}/edit`}>Update Issue</Link></Button>
+<EditIssueButton IssueId={issue.id} />
     </Box>
     </Grid>
   )
 }
 
-export default IssueDetails
+export default IssueDetailsPage
