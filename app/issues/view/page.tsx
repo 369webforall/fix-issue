7d6ceca55e9d3d6a72f1823ad74047902f07363e
369webforall <369webforall@gmail.com>
2024-04-30 10:56:40 +0545

    custom link

--- app/issues/view/page.tsx
+++ app/issues/view/page.tsx
@@ -3,7 +3,7 @@ import prisma from '@/prisma/client'
 import { Table } from '@radix-ui/themes';
 import IssueBadge from '@/app/components/IssueBadge';
 import delay from 'delay';
-import Link from 'next/link';
+import Link from '@/app/components/Link';
 const IssuePage = async() => {
   await delay(2000)
   const issues = await prisma.issue.findMany();
@@ -22,7 +22,7 @@ const IssuePage = async() => {
     {issues.map((issue)=>(
  <Table.Row key={issue.id}>
  <Table.Cell>
-  <Link href={`/issues/view/${issue.id}`} className='hover:text-purple-600'>{issue.title}</Link>
+  <Link href={`/issues/view/${issue.id}`}>{issue.title}</Link>
   <div className='block md:hidden'>
       <IssueBadge status={issue.status} />
   </div>
