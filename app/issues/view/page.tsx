de37bf548897224cfebe9413176410c3ac908e57
369webforall <369webforall@gmail.com>
2024-05-16 15:07:03 +0545

    order and filter

--- app/issues/view/page.tsx
+++ app/issues/view/page.tsx
@@ -1,28 +1,44 @@
 import prisma from '@/prisma/client';
 import { Table } from '@radix-ui/themes';
 import {IssueBadge, Link, ActionButton} from "../../components"
-import { Status } from '@prisma/client';
-const IssuePage = async({searchParams}:{searchParams:{status:Status}}) => {
+import { Status, Issue } from '@prisma/client';
+import NextLink from 'next/link';
+import { ArrowUp } from 'lucide-react';
+
+const IssuePage = async({searchParams}:{searchParams:{status:Status, orderBy:string}}) => {
   
   const statuses = Object.values(Status) 
 
-  const status = statuses.includes(searchParams.status) ? searchParams.status : undefined;
+  const status = statuses.includes(searchParams.status) ? searchParams.status :undefined;
+
+  const columns:{label:string; 
+    value: keyof Issue;
+  className?:string;
+   }[] = [
+    {label:'Title', value:'title'},
+    {label:'Status', value:'status', className:'hidden md:table-cell'},
+    {label:'CreatedAt', value:'createdAt', className:'hidden md:table-cell'}]
 
   const issues = await prisma.issue.findMany({
     where:{
       status: status
+    },
+    orderBy:{
+      [searchParams.orderBy]:'asc'
     }
   });
-  
+
   return (
     <div className='space-y-4 mt-4'>
       <ActionButton />
       <Table.Root variant='surface'>
   <Table.Header>
     <Table.Row>
-      <Table.ColumnHeaderCell>Title</Table.ColumnHeaderCell>
-      <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
-      <Table.ColumnHeaderCell className='hidden md:table-cell'>CreatedAt</Table.ColumnHeaderCell>
+    {columns.map((column)=>(
+        <Table.ColumnHeaderCell key={column.value} className={column.className}><NextLink href={{query:{...searchParams, orderBy:column.value}}}>{column.label}</NextLink>
+        {column.value === searchParams.orderBy && <ArrowUp className='inline'/>}
+        </Table.ColumnHeaderCell>
+      ))}
     </Table.Row>
   </Table.Header>
 
